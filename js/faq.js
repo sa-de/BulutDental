45665454c8f626d8a735a713d7ab98f442489bf0
Yusuf Demir <yussir4@gmail.com>
2018-09-14 22:25:43 +0300

    Transformİcon Başarıyla Kuruldu Ayarlandı

--- js/faq.js
+++ js/faq.js
@@ -4,8 +4,13 @@ if( jQuery(".toggle .toggle-title").hasClass('active') ){
   jQuery(".toggle .toggle-title").click(function(){
     if( jQuery(this).hasClass('active') ){
       jQuery(this).removeClass("active").closest('.toggle').find('.toggle-inner').slideUp(200);
+      jQuery(this).find('button').removeClass('tcon-transform');
+
     }
-    else{	jQuery(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
+    else{	
+      jQuery(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
+      console.log("aç");
+      jQuery(this).find('button').addClass('tcon-transform');
     }
   });
   
